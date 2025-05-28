8d68849592d8e50ec09ecc20f65fd506e250dc5c
SamuelKamikiMatsuoka <samuelmatsuokakamiki@gmail.com>
2025-05-28 08:51:00 -0300

    hello guys

--- Lição2.js
+++ Lição2.js
@@ -1,19 +1,11 @@
 let numeros = [];
 
-
-
-for(let index = 0; index<10; index++){
-    let numero = parseInt(prompt(`digite o  ${index+1} numero`)); 
-    numeros[index] = numero; 
-
-
+for (let index = 0; index < 10; index++) {
+  let numero = parseInt(prompt(`digite o  ${index + 1} numero`));
+  numeros[index] = numero;
 }
-for(let index = 0; index < 10 ; index++){
-
-    if (numeros[index]% 2 === 0){
-
-        console.log(`numero ${numeros[index]} posição ${index}`)
-
-
-    }
+for (let index = 0; index < 10; index++) {
+  if (numeros[index] % 2 === 0) {
+    console.log(`numero ${numeros[index]} posição ${index}`);
+  }
 }
