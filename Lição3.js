e141b1330d6d2fd98285c9dd62cbc64ab799a150
SamuelKamikiMatsuoka <samuelmatsuokakamiki@gmail.com>
2025-05-28 09:54:32 -0300

    hello world

--- Lição3.js
+++ Lição3.js
@@ -2,14 +2,12 @@ let NumerosLista = [];
 let numero;
 let contador = 0;
 
-
 for (let index = 0; index < 30; index++) {
   NumerosLista[index] = Math.floor(Math.random() * 15) + 1;
 }
 
-
-numero = parseInt(prompt("Digite um número (numero) para buscar no NumerosLista:"));
-
+numero = parseInt(prompt("Digite um número (numero) para buscar no NumerosLista:")
+);
 
 for (contagem = 0; contagem < 30; contagem++) {
   if (NumerosLista[contagem] === numero) {
